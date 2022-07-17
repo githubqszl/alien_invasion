d8a3005352b23722d9e965112954ddd3ad095563
githubqszl <4869a406451405@gmail.com>
2022-07-17 13:07:34 +0800

    12.6.1响应按键

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,20 @@ import sys
 
 import pygame
 
-def check_events():
+import ship
+
+
+def check_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+            #KEYDOWN指的是按下键盘
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                #向右移动飞船
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
     """更新屏幕上的图像，并切换到新屏幕"""
