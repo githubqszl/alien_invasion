06bc9beb2d9ebdf0e244dd2d0f134c65ba1440b9
githubqszl <4869a406451405@gmail.com>
2022-07-17 14:55:52 +0800

    12.8.1添加子弹设置，12.8.2创建Bullet类，12.8.3将子弹存储到编组中，12.8.4开火

--- settings.py
+++ settings.py
@@ -13,3 +13,9 @@ class Settings():
         #这样，每次移动1.5个像素，而不是1个像素
         self.ship_speed_factor = 1.5
 
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
+
