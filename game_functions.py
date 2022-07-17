81da78eb959dce8caa7d984d90e359ef5cb3caf5
githubqszl <4869a406451405@gmail.com>
2022-07-17 19:40:46 +0800

    13.3.3 创建外星人群

--- game_functions.py
+++ game_functions.py
@@ -5,6 +5,7 @@ import pygame
 import ship
 
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """响应按键"""
@@ -70,6 +71,24 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+def create_fleet(ai_settings, screen, aliens):
+    """创建外星人群"""
+    #创建一个外星人，并计算一行可容纳多少个外星人
+    #外星人间距为外星人宽度
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    #创建第一行外星人
+    for alien_number in range(number_aliens_x):
+        #创建一个外星人并将其加入当前行
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
 
 
 
