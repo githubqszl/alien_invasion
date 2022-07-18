398506991f2e31be8227a899f6e0969ff2a370e2
githubqszl <4869a406451405@gmail.com>
2022-07-18 17:46:36 +0800

    13.6.3 有外星人到达屏幕底端

--- game_functions.py
+++ game_functions.py
@@ -162,6 +162,16 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     #暂停
     sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """检查是否有外星人到达了屏幕底端"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #像飞船被撞到一样进行处理
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
+
 
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """
@@ -173,6 +183,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     #检测外星人和飞船之间的碰撞
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+
+    #检查是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 
 
